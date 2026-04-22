2bb40b81013abf144f84bb451bbbf457da157a87
Juniper Hallbick <pat.hallbick@outlook.com>
2026-04-21 21:09:44 -0400

    Add case for 1 field

--- creap.rkt
+++ creap.rkt
@@ -1,9 +1,7 @@
 (define l (string-split (call-trick `corn "list") "\n" #:repeat? #t))
 (define ll (- (length l) 1))
-(define li (+ (random ll) 1))
-(define c (list-ref l li))
-(define n (- (string-length c) 3))
-(define f (substring c 5 n))
+(define c (list-ref l (+ (random ll) 1)))
+(define f (substring c 5 (- (string-length c) 3)))
 
 (define o (if (string-contains? f " ")
     (string-append "reap \"" f "\"")
@@ -11,6 +9,9 @@
 ))
 
 (string-append 
-    "There are " (number->string ll) " fields.\n"
+    (if (= ll 1) 
+        "There is one field."
+        (string-append "There are " (number->string ll) " fields.\n")
+    )
     (call-trick 'corn o)
 )
