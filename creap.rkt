d1f5aadcc0231386fbd53a4f3676a73feae7172d
Juniper Hallbick <pat.hallbick@outlook.com>
2026-04-24 02:38:46 -0400

    Use lazier test for when quotes are needed

--- creap.rkt
+++ creap.rkt
@@ -1,7 +1,14 @@
 (define l (string-split (call-trick `corn "list") "\n" #:repeat? #t))
 (define ll (- (length l) 1))
 (define c (list-ref l (+ (random ll) 1)))
-(define f (substring c 5 (- (string-length c) 3)))
+
+(define (reap field)
+    (define res (call-trick 'corn (string-append "reap " field)))
+    (if (string-contains? res "doesn't exist!")
+        (call-trick 'corn (string-append "reap \"" field "\""))
+        res
+    )
+)
 
 (if (= ll 0)
     "There are no fields to reap!"
@@ -10,9 +17,6 @@
             "There is one field.\n"
             (string-append "There are " (number->string ll) " fields.\n")
         )
-        (call-trick 'corn (if (string-contains? f " ")
-            (string-append "reap \"" f "\"")
-            (string-append "reap " f)
-        ))
+        (reap (substring c 5 (- (string-length c) 3)))
     )
 )
