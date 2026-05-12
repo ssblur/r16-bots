bc55648d877df5e42df5d200ccc9f83e9be524fa
Juniper Hallbick <pat.hallbick@outlook.com>
2026-05-12 17:03:32 -0400

    Add seasons to rototiller

--- rototiller.rkt
+++ rototiller.rkt
@@ -2,6 +2,11 @@
 (define ll (- (length l) 1))
 (define c (list-ref l (+ (random ll) 1)))
 
+(if (read-storage 'guild)
+    (if (> ll 49) (write-storage 'guild #f) #f)
+    (if (< ll 1) (write-storage 'guild #t) #f)
+)
+
 (define (reap field)
     (define res (call-trick 'corn (string-append "reap " field)))
     (if (or (string-suffix? res "doesn't exist!") (string-suffix? res "halve your cooldown!"))
@@ -10,8 +15,12 @@
     )
 )
 
-(if (= ll 0)
-    "There are no fields to reap!"
+(if (read-storage 'guild)
+    (string-append 
+        "It's not harvest season! There are only " 
+        (number->string ll)
+        " fields. Harvest season will start once there are 50 or more fields."
+    )
     (string-append 
         (if (= ll 1) 
             "There is one field.\n"
